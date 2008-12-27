d4b1ac7cabc5f9ef691d63b6404c158f24526962
Andrey Shchekin <ashmind@gmail.com>
2008-12-27 12:56:31 +0000

    Added Type.IsSubclassOf generic extension.

--- AshMind.Extensions/AshMind.Extensions/ReflectionExtensions.cs
+++ AshMind.Extensions/AshMind.Extensions/ReflectionExtensions.cs
@@ -2,28 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace AshMind.Extensions
 {
-    public static class ReflectionExtensions
-    {
-        public static TAttribute[] GetCustomAttributes<TAttribute>(this MemberInfo member, bool inherit)
+    public static class ReflectionExtensions {
+        public static TAttribute[] GetCustomAttributes<TAttribute>(this ICustomAttributeProvider member, bool inherit) 
             where TAttribute : Attribute
         {
-            return (TAttribute[])member.GetCustomAttributes(typeof(TAttribute), true);
+            return (TAttribute[])member.GetCustomAttributes(typeof(TAttribute), inherit);
         }
 
-        public static TAttribute GetCustomAttribute<TAttribute>(this MemberInfo member, bool inherit)
+        public static TAttribute GetCustomAttribute<TAttribute>(this ICustomAttributeProvider member, bool inherit)
             where TAttribute : Attribute
         {
-            return Attribute.GetCustomAttribute(member, typeof(TAttribute), inherit) as TAttribute;
+            return member.GetCustomAttributes<TAttribute>(inherit).Single();
         }
 
-        public static TAttribute GetCustomAttribute<TAttribute>(this MemberInfo member)
+        public static TAttribute GetCustomAttribute<TAttribute>(this ICustomAttributeProvider member)
             where TAttribute : Attribute
         {
-            return Attribute.GetCustomAttribute(member, typeof(TAttribute)) as TAttribute;
+            return member.GetCustomAttributes<TAttribute>(false).Single();
+        }
+
+        public static bool IsSubclassOf<T>(this Type type) {
+            return type.IsSubclassOf(typeof(T));
         }
     }
 }
