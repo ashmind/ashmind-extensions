620d60c162b30473c0df260b99972f1eac037aae
Andrey Shchekin <ashmind@gmail.com>
2009-01-05 20:12:17 +0000

    Added extensions to IComparable<T>.

---
+++ AshMind.Extensions/AshMind.Extensions/ComparableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AshMind.Extensions {
+    public static class ComparableExtensions {
+        public static bool IsGreaterThan<TComparable, T>(this TComparable left, T right)
+            where TComparable : IComparable<T>
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool IsLesserThan<TComparable, T>(this TComparable left, T right)
+            where TComparable : IComparable<T>
+        {
+            return left.CompareTo(right) < 0;
+        }
+    }
+}
