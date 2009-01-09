6ca09e584a9145634a39cb8859553f7bdb00831e
Andrey Shchekin <ashmind@gmail.com>
2009-01-09 11:00:12 +0000

    * Added RemoveWhere to collections. * All new methods should have a documentation.

--- AshMind.Extensions/AshMind.Extensions/CollectionExtensions.cs
+++ AshMind.Extensions/AshMind.Extensions/CollectionExtensions.cs
@@ -1,20 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace AshMind.Extensions
-{
-    public static class CollectionExtensions
-    {
-        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> values)
-        {
+namespace AshMind.Extensions {
+    public static class CollectionExtensions {
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> values) {
             values.ForEach(collection.Add);
         }
 
-        public static void RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> values)
-        {
+        public static void RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> values) {
             values.ForEach(item => collection.Remove(item));
+        }
+
+        /// <summary>
+        /// Removes all elements that match the conditions defined by the specified predicate from the collection.
+        /// </summary>
+        /// <param name="collection">
+        /// The collection from which to remove items.
+        /// </param>
+        /// <param name="predicate">
+        /// The Func&lt;T, bool&gt; delegate that defines the conditions of the elements to remove.
+        /// </param>
+        /// <returns>The number of elements that were removed from the collection.</returns>
+        public static int RemoveWhere<T>(this ICollection<T> collection, Func<T, bool> predicate) {
+            var list = collection as IList<T>;
+            if (list != null)
+                return RemoveFromListWhere(list, predicate);
+
+            var set = collection as HashSet<T>;
+            if (set != null)
+                return set.RemoveWhere(predicate.AsPredicate());
+            
+            var itemsToRemove = new List<T>();
+            foreach (var item in collection) {
+                if (predicate(item))
+                    itemsToRemove.Add(item);
+            }
+
+            collection.RemoveRange(itemsToRemove);
+            return itemsToRemove.Count;
+        }
+
+        private static int RemoveFromListWhere<T>(IList<T> list, Func<T, bool> predicate) {
+            var removedCount = 0;
+            for (var i = list.Count - 1; i >= 0; i--) {
+                if (!predicate(list[i]))
+                    continue;
+
+                list.RemoveAt(i);
+                removedCount += 1;
+            }
+
+            return removedCount;
         }
     }
 }
---
+++ AshMind.Extensions/AshMind.Extensions/DelegateExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AshMind.Extensions {
+    /// <summary>
+    /// Provides a set of static (Shared in Visual Basic) methods for operations on delegates. 
+    /// </summary>
+    public static class DelegateExtensions {
+        /// <summary>.
+        /// Converts Func&lt;T, bool&gt; into a Predicate&lt;T&gt;.
+        /// </summary>
+        /// <param name="function">Function to convert.</param>
+        /// <returns>Predicate&lt;T&gt; identical to the original function.</returns>
+        public static Predicate<T> AsPredicate<T>(this Func<T, bool> function) {
+            return (Predicate<T>)Delegate.CreateDelegate(typeof(Predicate<T>), function.Target, function.Method);
+        }
+    }
+}
