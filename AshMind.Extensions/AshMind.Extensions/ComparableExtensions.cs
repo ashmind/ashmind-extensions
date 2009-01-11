2ae8b772b2e7832514f738be513e3ba7534b7304
Andrey Shchekin <ashmind@gmail.com>
2009-01-11 21:53:46 +0000

    Add IsBetween to IComparables/ints.

--- AshMind.Extensions/AshMind.Extensions/ComparableExtensions.cs
+++ AshMind.Extensions/AshMind.Extensions/ComparableExtensions.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace AshMind.Extensions {
+    /// <summary>
+    /// Provides a set of extension methods for operations on IComparable&lt;&gt;.
+    /// </summary>
     public static class ComparableExtensions {
         public static bool IsGreaterThan<TComparable, T>(this TComparable left, T right)
             where TComparable : IComparable<T>
@@ -14,6 +17,23 @@ namespace AshMind.Extensions {
             where TComparable : IComparable<T>
         {
             return left.CompareTo(right) < 0;
+        }
+
+        /// <summary>
+        /// Determines the specified value in between two other values, inclusive.
+        /// </summary>
+        /// <typeparam name="TComparable">Type of the value to check.</typeparam>
+        /// <typeparam name="T">Type that the value can be compared with.</typeparam>
+        /// <param name="value">The value to compare</param>
+        /// <param name="left">Minimum value that can cause true to be returned</param>
+        /// <param name="right">Maximum value that can cause true to be returned</param>
+        /// <returns>
+        /// True if the specified value is between the minimm and maximum; otherwise, false.
+        /// </returns>
+        public static bool IsBetween<TComparable, T>(this TComparable value, T left, T right)
+            where TComparable : IComparable<T> 
+        {
+            return !value.IsLesserThan(left) && !value.IsGreaterThan(right);
         }
     }
 }
---
+++ AshMind.Extensions/AshMind.Extensions/Int32Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AshMind.Extensions {
+    /// <summary>
+    /// Provides a set of extension methods for operations on Int32.
+    /// </summary>
+    public static class Int32Extensions {
+        /// <summary>
+        /// Determines the specified value in between two other values, inclusive.
+        /// </summary>
+        /// <param name="value">The value to compare</param>
+        /// <param name="left">Minimum value that can cause true to be returned</param>
+        /// <param name="right">Maximum value that can cause true to be returned</param>
+        /// <returns>
+        /// True if the specified value is between the minimm and maximum; otherwise, false.
+        /// </returns>
+        public static bool IsBetween(this int value, int left, int right) {
+            return value >= left && value <= right;
+        }
+    }
+}
