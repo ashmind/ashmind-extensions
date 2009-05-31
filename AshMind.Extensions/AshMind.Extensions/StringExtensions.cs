2f9c925919932c1132563458ea4b3aed11282c49
Andrey Shchekin <ashmind@gmail.com>
2009-05-31 14:04:19 +0000

    Added String.Contains extension method.

--- AshMind.Extensions/AshMind.Extensions/StringExtensions.cs
+++ AshMind.Extensions/AshMind.Extensions/StringExtensions.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace AshMind.Extensions {
+    /// <summary>
+    /// Provides a set of extension methods for operations on String.
+    /// </summary>
     public static class StringExtensions {
         public static bool IsNullOrEmpty(this string value) {
             return string.IsNullOrEmpty(value);
@@ -15,25 +18,18 @@ namespace AshMind.Extensions {
         public static string[] Split(this string value, params string[] separator) {
             return value.Split(separator, StringSplitOptions.None);
         }
-        
-        public static string Format(this string format, object arg0) {
-            return string.Format(format, arg0);
-        }
 
-        public static string Format(this string format, params object[] args) {
-            return string.Format(format, args);
-        }
-
-        public static string Format(this string format, IFormatProvider provider, params object[] args) {
-            return string.Format(provider, format, args);
-        }
-
-        public static string Format(this string format, object arg0, object arg1) {
-            return string.Format(format, arg0, arg1);
-        }
-
-        public static string Format(this string format, object arg0, object arg1, object arg2) {
-            return string.Format(format, arg0, arg1, arg2);
+        /// <summary>
+        /// Returns a value indicating whether the specified String object occurs within this string.
+        /// </summary>
+        /// <param name="original">The <see cref="String" /> value to be analyzed.</param>
+        /// <param name="value">The <see cref="String" /> object to seek.</param>
+        /// <param name="comparisonType">One of the <see cref="StringComparison" /> values that determines how this string and value are compared.</param>
+        /// <returns>
+        ///     <c>true</c> if the value parameter occurs within this string, or if value is the empty string (""); otherwise, <c>false</c>.
+        /// </returns>
+        public static bool Contains(this string original, string value, StringComparison comparisonType) {
+            return original.IndexOf(value, comparisonType) >= 0;
         }
     }
 }
