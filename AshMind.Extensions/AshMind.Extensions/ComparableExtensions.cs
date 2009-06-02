c46fae1d0230f2f71124072d236ab22321aee64a
Andrey Shchekin <ashmind@gmail.com>
2009-06-02 19:19:29 +0000

    Fixed xml comment typos and added some comments.

--- AshMind.Extensions/AshMind.Extensions/ComparableExtensions.cs
+++ AshMind.Extensions/AshMind.Extensions/ComparableExtensions.cs
@@ -7,12 +7,32 @@ namespace AshMind.Extensions {
     /// Provides a set of extension methods for operations on IComparable&lt;&gt;.
     /// </summary>
     public static class ComparableExtensions {
+        /// <summary>
+        /// Determines whether the specified value is greater than another value.
+        /// </summary>
+        /// <typeparam name="TComparable">Type of the value to check.</typeparam>
+        /// <typeparam name="T">Type that the value can be compared with.</typeparam>
+        /// <param name="left">The value to compare</param>
+        /// <param name="right">Value to compare with</param>
+        /// <returns>
+        /// True if <paramref name="left" /> is greater than <paramref name="right" />; otherwise, false.
+        /// </returns>
         public static bool IsGreaterThan<TComparable, T>(this TComparable left, T right)
             where TComparable : IComparable<T>
         {
             return left.CompareTo(right) > 0;
         }
 
+        /// <summary>
+        /// Determines whether the specified value is lesser than another value.
+        /// </summary>
+        /// <typeparam name="TComparable">Type of the value to check.</typeparam>
+        /// <typeparam name="T">Type that the value can be compared with.</typeparam>
+        /// <param name="left">The value to compare</param>
+        /// <param name="right">Value to compare with</param>
+        /// <returns>
+        /// True if <paramref name="left" /> is lesser than <paramref name="right" />; otherwise, false.
+        /// </returns>
         public static bool IsLesserThan<TComparable, T>(this TComparable left, T right)
             where TComparable : IComparable<T>
         {
@@ -20,7 +40,7 @@ namespace AshMind.Extensions {
         }
 
         /// <summary>
-        /// Determines the specified value is greater than or equal to another value.
+        /// Determines whether the specified value is greater than or equal to another value.
         /// </summary>
         /// <typeparam name="TComparable">Type of the value to check.</typeparam>
         /// <typeparam name="T">Type that the value can be compared with.</typeparam>
@@ -36,7 +56,7 @@ namespace AshMind.Extensions {
         }
 
         /// <summary>
-        /// Determines the specified value is lesset than or equal to another value.
+        /// Determines whether the specified value is lesser than or equal to another value.
         /// </summary>
         /// <typeparam name="TComparable">Type of the value to check.</typeparam>
         /// <typeparam name="T">Type that the value can be compared with.</typeparam>
@@ -52,7 +72,7 @@ namespace AshMind.Extensions {
         }
 
         /// <summary>
-        /// Determines the specified value is between two other values, inclusive.
+        /// Determines whether the specified value is between two other values, inclusive.
         /// </summary>
         /// <typeparam name="TComparable">Type of the value to check.</typeparam>
         /// <typeparam name="T">Type that the value can be compared with.</typeparam>
