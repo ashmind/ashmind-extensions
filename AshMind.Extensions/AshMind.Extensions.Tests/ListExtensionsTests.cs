adcc34c9ce5d3bfa71f5746f9c75905399d4546b
Andrey Shchekin <ashmind@gmail.com>
2009-06-08 17:51:13 +0000

    Added IList.AsReadOnly (by izobr). Added some unit tests.

---
+++ AshMind.Extensions/AshMind.Extensions.Tests/ListExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+using MbUnit.Framework;
+
+namespace AshMind.Extensions.Tests {
+    [TestFixture]
+    public class ListExtensionsTests {
+        [Test]
+        public void TestAsReadOnlyReturnsAllItemsFromTheOriginalList() {
+            var list = (new List<int> { 1, 2, 4 }) as IList<int>;
+            Assert.AreElementsEqual(list, list.AsReadOnly());
+        }
+
+        [Test]
+        public void TestAsReadOnlyReturnsSameInstanceForTheReadOnlyCollection() {
+            var collection = new ReadOnlyCollection<int>(new[] { 1, 2, 4 });
+            Assert.AreSame(collection, collection.AsReadOnly());
+        }
+    }
+}
---
+++ AshMind.Extensions/AshMind.Extensions/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace AshMind.Extensions {
+    /// <summary>
+    /// Provides a set of extension methods for operations on <see cref="IList&lt;T&gt;" />.
+    /// </summary>
+    public static class ListExtensions {
+        /// <summary>
+        /// Returns the input typed as <see cref="ReadOnlyCollection&lt;T&gt;" />. 
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="list" />.</typeparam>
+        /// <param name="list">The list to type as <see cref="ReadOnlyCollection&lt;T&gt;" /></param>
+        /// <returns>The input list typed as <see cref="ReadOnlyCollection&lt;T&gt;" />.</returns>
+        /// <remarks>
+        ///  For instances of <see cref="ReadOnlyCollection&lt;T&gt;" /> the same instance is returned.
+        /// </remarks>
+        public static ReadOnlyCollection<T> AsReadOnly<T>(this IList<T> list) {
+            return (list as ReadOnlyCollection<T>) ?? new ReadOnlyCollection<T>(list);
+        }
+    }
+}
