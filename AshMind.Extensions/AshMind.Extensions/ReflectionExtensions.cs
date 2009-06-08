97aa3144652f2a614ca9dc2f11a2b4158efddf28
Andrey Shchekin <ashmind@gmail.com>
2009-06-08 18:07:06 +0000

    Added some documentation to reflection extensions, cleaned them up a bit.

--- AshMind.Extensions/AshMind.Extensions/ReflectionExtensions.cs
+++ AshMind.Extensions/AshMind.Extensions/ReflectionExtensions.cs
@@ -3,15 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace AshMind.Extensions
-{
+namespace AshMind.Extensions {
+    /// <summary>
+    /// Provides a set of extension methods for operations on reflection classes.
+    /// </summary>
     public static class ReflectionExtensions {
+        /// <summary>
+        /// Gets the custom attributes of the specified type defined on this member.
+        /// </summary>
+        /// <typeparam name="TAttribute">The type of attribute to search for. Only attributes that are assignable to this type are returned.</typeparam>
+        /// <param name="member">The member which attributes will be retrieved.</param>
+        /// <param name="inherit">Specifies whether to search this member's inheritance chain to find the attributes.</param>
+        /// <returns>An array of custom attributes applied to this member, or an array with zero (0) elements if no attributes have been applied.</returns>
+        /// <exception cref="TypeLoadException">A custom attribute type cannot be loaded.</exception>
+        /// <exception cref="InvalidOperationException">This member belongs to a type that is loaded into the reflection-only context.</exception>
         public static TAttribute[] GetCustomAttributes<TAttribute>(this ICustomAttributeProvider member, bool inherit) 
             where TAttribute : Attribute
         {
             return (TAttribute[])member.GetCustomAttributes(typeof(TAttribute), inherit);
         }
 
+        /// <summary>
+        /// Gets the custom attributes of the specified type defined on this member.
+        /// </summary>
+        /// <typeparam name="TAttribute">The type of attribute to search for. Only attributes that are assignable to this type are returned.</typeparam>
+        /// <param name="member">The member which attributes will be retrieved.</param>
+        /// <returns>An array of custom attributes applied to this member, or an array with zero (0) elements if no attributes have been applied.</returns>
+        /// <exception cref="TypeLoadException">A custom attribute type cannot be loaded.</exception>
+        /// <exception cref="InvalidOperationException">This member belongs to a type that is loaded into the reflection-only context.</exception>
+        public static TAttribute[] GetCustomAttributes<TAttribute>(this ICustomAttributeProvider member) 
+            where TAttribute : Attribute
+        {
+            return (TAttribute[])member.GetCustomAttributes(typeof(TAttribute), true);
+        }
+        
+        /// <summary>
+        /// Gets the custom attributes of the specified type defined on this member.
+        /// </summary>
+        /// <param name="member">The member which attributes will be retrieved.</param>
+        /// <param name="attributeType">The type of attribute to search for. Only attributes that are assignable to this type are returned.</param>
+        /// <returns>An array of custom attributes applied to this member, or an array with zero (0) elements if no attributes have been applied.</returns>
+        /// <exception cref="TypeLoadException">A custom attribute type cannot be loaded.</exception>
+        /// <exception cref="NullReferenceException">If <paramref name="attributeType" /> is a null reference (Nothing in Visual Basic).</exception>
+        /// <exception cref="InvalidOperationException">This member belongs to a type that is loaded into the reflection-only context.</exception>
+        public static object[] GetCustomAttributes(this ICustomAttributeProvider member, Type attributeType) {
+            return member.GetCustomAttributes(attributeType, true);
+        }
+
+        /// <summary>
+        /// Returns an array containing all the custom attributes defined on this member.
+        /// </summary>
+        /// <param name="member">The member which attributes will be retrieved.</param>
+        /// <returns>An array that contains all the custom attributes, or an array with zero elements if no attributes are defined.</returns>
+        /// <exception cref="TypeLoadException">A custom attribute type cannot be loaded.</exception>
+        /// <exception cref="InvalidOperationException">This member belongs to a type that is loaded into the reflection-only context.</exception>
+        public static object[] GetCustomAttributes(this ICustomAttributeProvider member) {
+            return member.GetCustomAttributes(true);
+        }
+        
         public static TAttribute GetCustomAttribute<TAttribute>(this ICustomAttributeProvider member, bool inherit)
             where TAttribute : Attribute
         {
@@ -46,7 +95,7 @@ namespace AshMind.Extensions
             return type.IsSubclassOf(typeof(T));
         }
 
-        public static bool IsGenericDefinedBy(this Type type, Type otherType) {
+        public static bool IsGenericDefinedAs(this Type type, Type otherType) {
             if (!type.IsGenericType)
                 return false;
 
