8e2f836c397218fa92adc84404e71c84e15af3a3
Andrey Shchekin <ashmind@gmail.com>
2009-06-28 08:05:30 +0000

    Added some transactional file operations.

---
+++ AshMind.Framework/IO/Transactional/File.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using AshMind.Framework.IO.Transactional.Internal;
+
+namespace AshMind.Framework.IO.Transactional {
+    public static class File {
+        public static FileStream Open(string path, FileMode mode) {
+            return File.Open(path, mode, (mode == FileMode.Append) ? FileAccess.Write : FileAccess.ReadWrite, FileShare.None);
+        }
+
+        public static FileStream Open(string path, FileMode mode, FileAccess access) {
+            return File.Open(path, mode, access, FileShare.None);
+        }
+
+        public static FileStream Open(string path, FileMode mode, FileAccess access, FileShare share) {
+            if (!KtmTransactionHandle.IsAvailable)
+                return System.IO.File.Open(path, mode, access, share);
+
+            using (var transactionHandle = KtmTransactionHandle.Get()) {
+                var nativeMode = ToNative(mode);
+                var nativeShare = ToNative(share);
+                var nativeAccess = ToNative(access);
+
+                // Create the transacted file using P/Invoke.
+                var hFile = NativeMethods.CreateFileTransacted(
+                    path,
+                    nativeAccess,
+                    nativeShare,
+                    IntPtr.Zero,
+                    nativeMode,
+                    0,
+                    IntPtr.Zero,
+                    transactionHandle,
+                    IntPtr.Zero,
+                    IntPtr.Zero
+                );
+
+                // Throw an exception if an error occured.
+                if (hFile.IsInvalid)
+                    HandleNativeError(Marshal.GetLastWin32Error());
+
+                // Return a FileStream created using the 
+                // transacted file's handle.
+                var stream = new FileStream(hFile, access);
+                return stream;
+            }
+        }
+
+        public static void Move(string sourceFileName, string destFileName) {
+            if (!KtmTransactionHandle.IsAvailable)
+                System.IO.File.Move(sourceFileName, destFileName);
+
+            using (var transactionHandle = KtmTransactionHandle.Get()) {
+                var moved = NativeMethods.MoveFileTransacted(
+                    sourceFileName, destFileName, IntPtr.Zero, IntPtr.Zero, 0, transactionHandle
+                );
+
+                if (!moved)
+                    HandleNativeError(Marshal.GetLastWin32Error());
+            }
+        }
+
+        public static void Copy(string sourceFileName, string destFileName) {
+            File.Copy(sourceFileName, destFileName, false);
+        }
+
+        public static void Copy(string sourceFileName, string destFileName, bool overwrite) {
+            if (!KtmTransactionHandle.IsAvailable)
+                System.IO.File.Copy(sourceFileName, destFileName, overwrite);
+
+            using (var transactionHandle = KtmTransactionHandle.Get()) {
+                var cancel = false;
+                var flags = overwrite ? 0 : NativeMethods.CopyFileFlags.COPY_FILE_FAIL_IF_EXISTS;
+                var copied = NativeMethods.CopyFileTransacted(
+                    sourceFileName, destFileName, IntPtr.Zero, IntPtr.Zero, ref cancel, flags, transactionHandle
+                );
+
+                if (!copied)
+                    HandleNativeError(Marshal.GetLastWin32Error());
+            }
+        }
+
+        public static void Delete(string path) {
+            if (!KtmTransactionHandle.IsAvailable)
+                System.IO.File.Delete(path);
+
+            using (var transactionHandle = KtmTransactionHandle.Get()) {
+                var deleted = NativeMethods.DeleteFileTransacted(path, transactionHandle);
+                if (!deleted)
+                    HandleNativeError(Marshal.GetLastWin32Error());
+            }
+        }
+
+        private static void HandleNativeError(int error) {
+            throw new System.ComponentModel.Win32Exception(error);
+        }
+
+        private static NativeMethods.FileMode ToNative(FileMode mode) {
+            if (mode == FileMode.Append)
+                return (NativeMethods.FileMode)(int)FileMode.OpenOrCreate;
+
+            return (NativeMethods.FileMode)(int)mode;
+        }
+
+        private static NativeMethods.FileAccess ToNative(FileAccess access) {
+            return access == FileAccess.Read 
+                 ? NativeMethods.FileAccess.GENERIC_READ
+                 : NativeMethods.FileAccess.GENERIC_WRITE;
+        }
+
+        private static NativeMethods.FileShare ToNative(FileShare share) {
+            return (NativeMethods.FileShare)(int)share;
+        }
+
+    }
+}
---
+++ AshMind.Framework/IO/Transactional/NativeMethods.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+
+using AshMind.Framework.IO.Transactional.Internal;
+using Microsoft.Win32.SafeHandles;
+
+namespace AshMind.Framework.IO.Transactional {
+    // ReSharper disable InconsistentNaming
+    [SuppressUnmanagedCodeSecurity]
+    internal static class NativeMethods {
+        [Flags]
+        public enum FileShare {
+            FILE_SHARE_NONE = 0x00,
+            FILE_SHARE_READ = 0x01,
+            FILE_SHARE_WRITE = 0x02,
+            FILE_SHARE_DELETE = 0x04
+        }
+
+        public enum FileMode {
+            CREATE_NEW = 1,
+            CREATE_ALWAYS = 2,
+            OPEN_EXISTING = 3,
+            OPEN_ALWAYS = 4,
+            TRUNCATE_EXISTING = 5
+        }
+
+        public enum FileAccess {
+            GENERIC_READ = unchecked((int)0x80000000),
+            GENERIC_WRITE = 0x40000000
+        }
+
+        [Flags]
+        internal enum CopyFileFlags : uint {
+            COPY_FILE_FAIL_IF_EXISTS = 0x00000001,
+            COPY_FILE_RESTARTABLE = 0x00000002,
+            COPY_FILE_OPEN_SOURCE_FOR_WRITE = 0x00000004,
+            COPY_FILE_ALLOW_DECRYPTED_DESTINATION = 0x00000008,
+            COPY_FILE_COPY_SYMLINK = 0x00000800
+        }
+
+        [Flags]
+        public enum MoveFileFlags : uint {
+            MOVEFILE_REPLACE_EXISTING = 0x00000001,
+            MOVEFILE_COPY_ALLOWED = 0x00000002,
+            MOVEFILE_DELAY_UNTIL_REBOOT = 0x00000004,
+            MOVEFILE_WRITE_THROUGH = 0x00000008,
+            MOVEFILE_CREATE_HARDLINK = 0x00000010,
+            MOVEFILE_FAIL_IF_NOT_TRACKABLE = 0x00000020
+        }
+
+        // Win32 Error codes.
+        internal const int ERROR_SUCCESS = 0;
+        internal const int ERROR_FILE_NOT_FOUND = 2;
+        internal const int ERROR_NO_MORE_FILES = 18;
+        internal const int ERROR_RECOVERY_NOT_NEEDED = 6821;
+
+        [DllImport("kernel32.dll", EntryPoint = "CreateFileTransacted", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern SafeFileHandle CreateFileTransacted(
+            [In] string lpFileName,
+            [In] NativeMethods.FileAccess dwDesiredAccess,
+            [In] NativeMethods.FileShare dwShareMode,
+            [In] IntPtr lpSecurityAttributes,
+            [In] NativeMethods.FileMode dwCreationDisposition,
+            [In] int dwFlagsAndAttributes,
+            [In] IntPtr hTemplateFile,
+            [In] KtmTransactionHandle hTransaction,
+            [In] IntPtr pusMiniVersion,
+            [In] IntPtr pExtendedParameter
+        );
+
+        [DllImport("kernel32.dll", EntryPoint = "MoveFileTransacted", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool MoveFileTransacted(
+            [In] string lpExistingFileName,
+            [In] string lpNewFileName,
+            [In] IntPtr lpProgressRoutine,
+            [In] IntPtr lpData,
+            [In] MoveFileFlags dwFlags,
+            [In] KtmTransactionHandle hTransaction
+        );
+
+        [DllImport("kernel32.dll", EntryPoint = "CopyFileTransacted", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool CopyFileTransacted(
+            [In] string lpExistingFileName,
+            [In] string lpNewFileName,
+            [In] IntPtr lpProgressRoutine,
+            [In] IntPtr lpData,
+            [In] [MarshalAs(UnmanagedType.Bool)] ref bool pbCancel,
+            [In] CopyFileFlags dwCopyFlags,
+            [In] KtmTransactionHandle hTransaction
+        );
+
+        [DllImport("kernel32.dll", EntryPoint = "DeleteFileTransacted", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool DeleteFileTransacted(
+            [In] string lpFileName,
+            [In] KtmTransactionHandle hTransaction
+        );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool CloseHandle([In] IntPtr handle);
+     
+    }
+    // ReSharper restore InconsistentNaming
+}
