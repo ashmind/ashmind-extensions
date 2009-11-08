8f2c9fea6e1aed0e4168ef420d8d09161f87747d
Andrey Shchekin <ashmind@gmail.com>
2009-11-08 20:52:51 +0000

    Added Predicate.AsFunc. Added tests on DelegateExtensions. Updated references to the current Gallio.

---
+++ AshMind.Extensions/AshMind.Extensions.Tests/DelegateExtensionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MbUnit.Framework;
+
+namespace AshMind.Extensions.Tests {
+    [TestFixture]
+    public class DelegateExtensionTests {
+        [Test]
+        public void TestAsPredicate() {
+            Func<string, bool> func = x => x == "test";
+            var predicate = func.AsPredicate();
+
+            Assert.AreSame(func.Target, predicate.Target);
+            Assert.AreSame(func.Method, predicate.Method);
+        }
+
+        [Test]
+        public void TestAsFunc() {
+            Predicate<string> predicate = x => x == "test";
+            var func = predicate.AsFunc();
+
+            Assert.AreSame(predicate.Target, func.Target);
+            Assert.AreSame(predicate.Method, func.Method);
+        }
+    }
+}
--- AshMind.Extensions/AshMind.Extensions/DelegateExtensions.cs
+++ AshMind.Extensions/AshMind.Extensions/DelegateExtensions.cs
@@ -8,12 +8,21 @@ namespace AshMind.Extensions {
     /// </summary>
     public static class DelegateExtensions {
         /// <summary>
-        /// Converts Func&lt;T, bool&gt; into a Predicate&lt;T&gt;.
+        /// Converts Func&lt;T, bool&gt; into a <see cref="Predicate&lt;T&gt;" />.
         /// </summary>
-        /// <param name="function">Function to convert.</param>
+        /// <param name="function">A function to convert.</param>
         /// <returns>Predicate&lt;T&gt; identical to the original function.</returns>
         public static Predicate<T> AsPredicate<T>(this Func<T, bool> function) {
             return (Predicate<T>)Delegate.CreateDelegate(typeof(Predicate<T>), function.Target, function.Method);
+        }
+
+        /// <summary>
+        /// Converts <see cref="Predicate&lt;T&gt;" /> into a Func&lt;T, bool&gt;.
+        /// </summary>
+        /// <param name="predicate">A predicate to convert.</param>
+        /// <returns>Func&lt;T&gt; identical to the original predicate.</returns>
+        public static Func<T, bool> AsFunc<T>(this Predicate<T> predicate) {
+            return (Func<T, bool>)Delegate.CreateDelegate(typeof(Func<T, bool>), predicate.Target, predicate.Method);
         }
     }
 }
